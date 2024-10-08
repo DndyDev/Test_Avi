11c645deb50f47137065092e440cce1f70b9f97b
Sergey <sirtikhomirov@gmail.com>
2024-10-08 23:47:54 +0300

    caching

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -3,35 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Button : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler, IPointerEnterHandler {
-    bool on;
-    Animator animator;
+[RequireComponent(typeof(Animator))]
+public class Button : MonoBehaviour, IPointerClickHandler {
+    [SerializeField] private string anmitorParametr = "anim";
+    [SerializeField] private LoadSprites loader;
+    private bool on;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     public void OnPointerClick(PointerEventData eventData) {
-        Debug.Log("Click");
         on = true;
     }
 
-    public void OnPointerDown(PointerEventData eventData) {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnPointerEnter(PointerEventData eventData) {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnPointerExit(PointerEventData eventData) {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnPointerUp(PointerEventData eventData) {
-        throw new System.NotImplementedException();
-    }
 
     void Update() {
         if (on) {
-            animator = GetComponent<Animator>();
-            animator.SetBool("anim", true);
-            GameObject.Find("Panel").GetComponent<LoadSprites>().load();
+            animator.SetBool(anmitorParametr, true);
+            loader.load();
         }
     }
 }
